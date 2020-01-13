5744c0a7a666b86a5edc4920d5153ab430d2b6b3
bebaek <bebaek@gmail.com>
2020-01-12 19:58:51 -0600

    Add minimal asset history view

--- assetlogger/views.py
+++ assetlogger/views.py
@@ -1,5 +1,12 @@
+from django.views import generic
 from django.http import HttpResponse
+
+from assetlogger.models import AssetDate
 
 
 def index(request):
     return HttpResponse('Hello, world')
+
+
+class AssetDateListView(generic.ListView):
+    model = AssetDate
