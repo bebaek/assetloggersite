682f1455c4ecdab8343e46d9dc4ce4571695f7d1
bebaek <bebaek@gmail.com>
2020-01-12 21:44:23 -0600

    Revise setup-devenv.sh to preinstall wheel

--- ci/setup-devenv.sh
+++ ci/setup-devenv.sh
@@ -10,10 +10,16 @@ pkgs=(django flake8 ipython)
 
 # Create virtual environment. python3-venv is required.
 # Use _venv/. Spacemacs cannot even handle the presence of .venv/.
+echo Creating virtual environment...
 python3 -m venv --clear ~/_venv/assetlogger
 . ~/_venv/assetlogger/bin/activate
 
+# Installing wheel first helps install certain packages (e.g., backcall)
+echo Installing wheel by pip first...
+pip3 install wheel
+
 # Install support packages
+echo Installing required pip packages...
 if [ ${#pkgs[@]} -ne 0 ]; then
     pip3 install ${pkgs[@]}
 fi
