e082ecd6f9b07217a60e88b264881e18d976a391
bebaek <bebaek@gmail.com>
2020-01-25 17:37:54 -0600

    Use hyphen for urls

--- assetlogger/urls.py
+++ assetlogger/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('assets/', views.AssetListView.as_view(), name='assets'),
-    path('asset/create', views.create_asset, name='create_asset'),
-    path('asset_instance/create', views.create_asset_instance,
-         name='create_asset_instance'),
+    path('asset/create', views.create_asset, name='create-asset'),
+    path('asset-instance/create', views.create_asset_instance,
+         name='create-asset-instance'),
     path('asset_date/create', views.create_asset_date,
-         name='create_asset_date'),
+         name='create-asset-date'),
 ]
